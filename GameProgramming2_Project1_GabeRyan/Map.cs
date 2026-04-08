78142f2155bf4f37a601dc061f7472bbb75798e9
Terloaf <greenyryan2501@gmail.com>
2026-04-08 09:40:51 -0300

    Added Colour to the map

--- GameProgramming2_Project1_GabeRyan/Map.cs
+++ GameProgramming2_Project1_GabeRyan/Map.cs
@@ -16,9 +16,6 @@ namespace GameProgramming2_Project1_GabeRyan
 
         public bool[,] _occupiedSpaces;
 
-        int _mapOffset = -1;
-
-
 
 
         
@@ -47,16 +44,40 @@ namespace GameProgramming2_Project1_GabeRyan
             _occupiedSpaces[position._x, position._y] = state;
         }
 
+
         public void DisplayMap()
         {
-            Console.SetCursorPosition(0, 0);
+           
+
             for (int i = 0; i < _map.Length; i++)
             {
+                Console.SetCursorPosition(0, i);
+
+                for (int j = 0; j < _map[i].Length; j++)
+                {
+                    if (_map[i][j] == '`')
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+                    else if (_map[i][j] == '~')
+                    {
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                    }
+                    else if (_map[i][j] == '^')
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    }
+                    else if (_map[i][j] == '#')
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
+                    }
+                    Console.Write(_map[i][j]);
+                    Console.ResetColor();
+                }
+
+                
 
 
-
-
-                Console.WriteLine(_map[i]);
 
 
             }
